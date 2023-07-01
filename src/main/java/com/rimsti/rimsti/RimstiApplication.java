1cba9502483686b3e647d4d957a6874088322913
HutaoMain <alimohamedalcantara@gmail.com>
2023-07-02 00:16:17 +0800

    add component scan

--- src/main/java/com/rimsti/rimsti/RimstiApplication.java
+++ src/main/java/com/rimsti/rimsti/RimstiApplication.java
@@ -2,8 +2,10 @@ package com.rimsti.rimsti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.rimsti.rimsti")
 public class RimstiApplication {
 
 	public static void main(String[] args) {
