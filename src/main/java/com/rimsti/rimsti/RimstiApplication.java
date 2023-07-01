eddde972b7aef203732cc388d91b371f7782028f
HutaoMain <alimohamedalcantara@gmail.com>
2023-07-02 00:44:53 +0800

    add enableJpaRepository

--- src/main/java/com/rimsti/rimsti/RimstiApplication.java
+++ src/main/java/com/rimsti/rimsti/RimstiApplication.java
@@ -2,10 +2,11 @@ package com.rimsti.rimsti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableJpaRepositories(basePackages = "com.rimsti.rimsti.repository")
 @ComponentScan(basePackages = "com.rimsti.rimsti")
 public class RimstiApplication {
