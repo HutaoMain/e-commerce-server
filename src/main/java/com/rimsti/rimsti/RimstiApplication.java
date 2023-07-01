728154d55bf0fe4e0015a383590c3a16dece7f82
HutaoMain <alimohamedalcantara@gmail.com>
2023-07-01 21:13:40 +0800

    add datasource auto configuration

--- src/main/java/com/rimsti/rimsti/RimstiApplication.java
+++ src/main/java/com/rimsti/rimsti/RimstiApplication.java
@@ -2,8 +2,9 @@ package com.rimsti.rimsti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class RimstiApplication {
 
 	public static void main(String[] args) {
