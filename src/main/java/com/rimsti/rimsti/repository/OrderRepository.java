0d95ef0bcb70a965a1bcffb4401fa736f21c6afd
HutaoMain <alimohamedalcantara@gmail.com>
2023-07-01 13:38:02 +0800

    change order repository instead of native query, query to the object inside the project

--- src/main/java/com/rimsti/rimsti/repository/OrderRepository.java
+++ src/main/java/com/rimsti/rimsti/repository/OrderRepository.java
@@ -1,9 +1,7 @@
 package com.rimsti.rimsti.repository;
 
 import com.rimsti.rimsti.model.Order;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,15 +11,14 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query(value = "SELECT total_price, date_now, status, SUM(total_price) as totalPrice FROM dbecommerce.orders where status = \"Completed\" group by date_now;", nativeQuery = true)
+    interface sumOfTotalPrice{
+        Double getTotalPrice();
+        LocalDate getDateNow();
+        String getStatus();
+    }
+
+    @Query("SELECT SUM(o.totalPrice) as totalPrice, o.dateNow, o.status FROM Order o WHERE o.status = 'Completed' GROUP BY o.dateNow")
     List<sumOfTotalPrice> getByDate();
 
     List<Order> findByStatus(String status);
-
-    interface sumOfTotalPrice{
-        Double getTotalPrice();
-        LocalDate getDate_now();
-        String getStatus();
-
-    }
 }
